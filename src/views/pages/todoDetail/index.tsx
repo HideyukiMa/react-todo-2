06dc8da1a2a1e1659799f37d0cf54956f7556565
阿部直也 <n.abe@gemcook.com>
2023-01-10 18:07:46 +0900

    Todo詳細ページの作成完了

--- src/views/pages/todoDetail/index.tsx
+++ src/views/pages/todoDetail/index.tsx
@@ -1,8 +1,12 @@
 import React, { FC } from 'react';
 import TodoDetailUI from './presenter';
+import sampleData from './sampleData.json';
 
 const TodoDetail: FC = () => {
-	return <TodoDetailUI />;
+	const { id, title, details, isDone } = sampleData;
+	return (
+		<TodoDetailUI id={id} title={title} details={details} isDone={isDone} />
+	);
 };
 
 export default TodoDetail;
--- src/views/pages/todoDetail/presenter.tsx
+++ src/views/pages/todoDetail/presenter.tsx
@@ -1,12 +1,67 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 import paths from '../../../config/paths';
 import Header from '../../components/header';
+import classes from './presenter.module.scss';
+import { Props } from './types';
 
-const TodoDetailUI: FC = () => {
+const TodoDetailUI: FC<Props> = ({ id, title, details, isDone }) => {
 	return (
-		<div>
+		<div className={classes.todoDetail}>
 			<Header title="Todo詳細" path={paths.todoList} buttonText="一覧に戻る" />
-			TodoListUI
+			<div className={classes.contents}>
+				<div className={`${classes.content} ${classes.input}`}>
+					<label className={classes.label}>タイトル</label>
+					<input className={classes.inputValue} value={title} disabled />
+				</div>
+				<div className={`${classes.content} ${classes.textarea}`}>
+					<label className={classes.label}>詳細</label>
+
+					<textarea
+						className={classes.textareaValue}
+						// @ts-expect-error
+						value={details}
+						disabled
+					/>
+				</div>
+				<div className={classes.radio}>
+					<input
+						id="true"
+						type="radio"
+						name="isDone"
+						value={1}
+						checked={isDone}
+						disabled
+						className={classes.radioInput}
+					/>
+					<label htmlFor="true" className={classes.radioLabel}>
+						完了
+					</label>
+					<input
+						id="false"
+						type="radio"
+						name="isDone"
+						value={0}
+						checked={!isDone}
+						disabled
+						className={classes.radioInput}
+					/>
+					<label htmlFor="false" className={classes.radioLabel}>
+						未完了
+					</label>
+				</div>
+				<div className={classes.buttonArea}>
+					<Link to={`/edit-todo/${id}`}>
+						<button className={`${classes.button} ${classes.edit}`}>
+							編集する
+						</button>
+					</Link>
+
+					<button className={`${classes.button} ${classes.delete}`}>
+						削除する
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 };
