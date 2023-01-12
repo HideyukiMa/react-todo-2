04431b9c215caf5163c62b9bf0ba28c654e1ecea
阿部直也 <n.abe@gemcook.com>
2023-01-12 15:17:14 +0900

    編集画面にreact-hook-dormを追加

--- src/views/pages/editTodo/presenter.tsx
+++ src/views/pages/editTodo/presenter.tsx
@@ -4,37 +4,57 @@ import Header from '../../components/header';
 import classes from './presenter.module.scss';
 import { Props } from './types';
 
-const EditTodoUI: FC<Props> = ({ id, title, details, isDone }) => {
-	console.log(isDone);
-
+const EditTodoUI: FC<Props> = ({
+	id,
+	title,
+	details,
+	isDone,
+	register,
+	handleSubmit,
+	errors,
+	onSubmit
+}) => {
 	return (
 		<div className={classes.editTodo}>
 			<Header title="Todo編集" path={paths.todoList} buttonText="一覧に戻る" />
-			<form className={classes.form}>
+			<form onSubmit={handleSubmit(onSubmit)} className={classes.form}>
 				<div className={`${classes.content} ${classes.input}`}>
 					<label className={classes.label}>タイトル</label>
-					<input className={classes.inputValue} value={title} />
+					<input
+						{...register('title', { required: true })}
+						defaultValue={title}
+						className={classes.inputValue}
+					/>
+					{errors.title != null && (
+						<span className={classes.error}>入力必須項目です。</span>
+					)}
 				</div>
 				<div className={`${classes.content} ${classes.textarea}`}>
 					<label className={classes.label}>詳細</label>
-					{/* @ts-expect-error */}
-					<textarea className={classes.textareaValue} value={details} />
+					<textarea
+						{...register('details')}
+						// @ts-expect-error
+						defaultValue={details}
+						className={classes.textareaValue}
+					/>
 				</div>
 				<div className={classes.radio}>
 					<input
+						{...register('isDone')}
 						id="true"
 						type="radio"
 						name="isDone"
-						value={1}
-						checked={isDone}
+						value="完了"
+						defaultChecked={isDone}
 					/>
 					<label htmlFor="true">完了</label>
 					<input
+						{...register('isDone')}
 						id="false"
 						type="radio"
 						name="isDone"
-						value={0}
-						checked={!isDone}
+						value="未完了"
+						defaultChecked={!isDone}
 					/>
 					<label htmlFor="false">未完了</label>
 				</div>
--- src/views/pages/editTodo/types.ts
+++ src/views/pages/editTodo/types.ts
@@ -1,3 +1,25 @@
 import { Todo } from '../../../types/todo';
+import {
+	FieldErrorsImpl,
+	SubmitHandler,
+	UseFormHandleSubmit,
+	UseFormRegister
+} from 'react-hook-form';
 
-export interface Props extends Todo {}
+export interface Props extends Todo {
+	register: UseFormRegister<EditInputs>;
+	handleSubmit: UseFormHandleSubmit<EditInputs>;
+	errors: Partial<
+		FieldErrorsImpl<{
+			title: string;
+			details: string;
+		}>
+	>;
+	onSubmit: SubmitHandler<EditInputs>;
+}
+
+export interface EditInputs {
+	title: string;
+	details?: string;
+	isDone: boolean;
+}
