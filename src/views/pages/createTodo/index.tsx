627b12ce0c7842245412679356f17271367c9379
阿部直也 <n.abe@gemcook.com>
2023-01-31 12:10:07 +0900

    createTodoの処理完成

--- src/views/pages/createTodo/index.tsx
+++ src/views/pages/createTodo/index.tsx
@@ -1,16 +1,27 @@
 import React, { FC } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import paths from '../../../config/paths';
+import { useAppDispatch } from '../../../redux/hooks';
+import { createTodoAsync } from '../../../redux/slices/todo/todoSlice';
 import CreateTodoUI from './presenter';
 import { CreateInputs } from './types';
 
 const CreateTodo: FC = () => {
+	const navigate = useNavigate();
+	const dispatch = useAppDispatch();
 	const {
 		register,
 		handleSubmit,
 		formState: { errors }
 	} = useForm<CreateInputs>();
-	const onSubmit: SubmitHandler<CreateInputs> = (data): void => {
-		console.log(data);
+	const onSubmit: SubmitHandler<CreateInputs> = async (data): Promise<void> => {
+		try {
+			await dispatch(createTodoAsync(data));
+			navigate(paths.todoList);
+		} catch (error) {
+			alert('Todoの投稿に失敗しました。');
+		}
 	};
 	return (
 		<CreateTodoUI
