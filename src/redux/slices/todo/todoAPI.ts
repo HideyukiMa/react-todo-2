3aa5db74ce349d27e5eb383dff64bc3a7e2ac520
阿部直也 <n.abe@gemcook.com>
2023-01-31 14:06:34 +0900

    editTodo機能の完成

--- src/redux/slices/todo/todoAPI.ts
+++ src/redux/slices/todo/todoAPI.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { Todo } from '../../../types/todo';
 import { CreateInputs } from '../../../views/pages/createTodo/types';
+import { EditInputs } from '../../../views/pages/editTodo/types';
 
 export const todoApi = axios.create({
 	baseURL: 'http://localhost:8080/todo'
@@ -35,6 +36,27 @@ export const getTodoListAPI = async (): Promise<Todo[] | undefined> => {
 export const getTodoAPI = async (todoId: number): Promise<Todo | undefined> => {
 	try {
 		const response = await todoApi.get(`/${todoId}`);
+		const todo: Todo = response.data;
+		return todo;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export const editTodoAPI = async (
+	args: EditInputs
+): Promise<Todo | undefined> => {
+	try {
+		const { todoId, title, details, isDone } = args;
+		const convertedIsDone: boolean = isDone === '完了';
+		console.log(convertedIsDone);
+
+		const response = await todoApi.patch(`/`, {
+			todoId,
+			title,
+			details,
+			isDone: convertedIsDone
+		});
 		const todo: Todo = response.data;
 		return todo;
 	} catch (error) {
--- src/views/pages/editTodo/index.tsx
+++ src/views/pages/editTodo/index.tsx
@@ -1,30 +1,60 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { useParams } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
+import {
+	editTodoAsync,
+	getTodoAsync,
+	selectTodoDetail
+} from '../../../redux/slices/todo/todoSlice';
 import EditTodoUI from './presenter';
-import sampleData from './sampleData.json';
 import { EditInputs } from './types';
 
 const EditTodo: FC = () => {
+	const dispatch = useAppDispatch();
+	const todo = useAppSelector(selectTodoDetail);
+	const { todoId } = useParams();
 	const {
 		register,
 		handleSubmit,
 		formState: { errors }
 	} = useForm<EditInputs>();
-	const onSubmit: SubmitHandler<EditInputs> = (data): void => {
-		console.log(data);
+	const onSubmit: SubmitHandler<EditInputs> = async (data): Promise<void> => {
+		try {
+			const args = {
+				todoId: Number(todoId),
+				title: data.title,
+				details: data.details,
+				isDone: data.isDone
+			};
+			await dispatch(editTodoAsync(args));
+			alert('Todoの編集に成功しました。');
+		} catch (error) {
+			alert('Todoの編集に失敗しました。');
+		}
 	};
-	const { id, title, details, isDone } = sampleData;
-	return (
+
+	useEffect(() => {
+		const getTodo = async (): Promise<void> => {
+			try {
+				await dispatch(getTodoAsync(Number(todoId)));
+			} catch (error) {
+				alert('Todoの取得に失敗しました');
+			}
+		};
+		void getTodo();
+	}, [todoId]);
+
+	return todo?.id !== 0 ? (
 		<EditTodoUI
-			id={id}
-			title={title}
-			details={details}
-			isDone={isDone}
+			todo={todo}
 			register={register}
 			handleSubmit={handleSubmit}
 			errors={errors}
 			onSubmit={onSubmit}
 		/>
+	) : (
+		<div>Loading...</div>
 	);
 };
 
